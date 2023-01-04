24e6758dd7c807a91ece747d432550545619517f
ARod88 <alma.rodriguez72@yahoo.com>
2023-01-03 20:18:38 -0800

    adding window prompts

---
+++ index.js
@@ -0,0 +1,28 @@
+let response = window.prompt('Question: Do you head left or right?') // right or left window
+console.log(response.toLowerCase());
+
+if(response ==='left'){
+    console.log('RESPONSE HOLDS LEFT');
+
+    let response2 = window.prompt('You come across a stray cat.It scampers off down a small hole, just large enough for you to crawl through it. Do you follow it, or continue on your path?');
+    console.log(response2);
+
+ if(response2 === 'follow')
+    console.log('follow')
+
+    let response3 = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven');
+    console.log(response3)
+if(response3 === 'stay')
+    console.log(response3)
+
+    let response4 = window.prompt('You live happily amongst the cats for the rest of your days')
+if(response3 = 'stay');
+console.log(response3)
+if(response4 ==='spread the word')
+console.log(response4)
+let response5= window.prompt('After leaving the cat colony, you are never able to find it again; without proof, no one believes your story, which passes into legend nonetheless.')
+}
+
+
+
+    
